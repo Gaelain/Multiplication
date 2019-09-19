dd9766ea2e794ef9965965692cfe08f2faad0454
bep3 <martin.curtet@gmail.com>
2019-09-19 15:25:31 +0200

    boucle pour les liens ajoutee

--- index.php
+++ index.php
@@ -8,15 +8,13 @@
 </head>
 <body>
     <h1>Table de Multiplication :</h1>
-    <p><a href="table.php?table=1">Table de 1</a></p>
-    <p><a href="table.php?table=2">Table de 2</a></p>
-    <p><a href="table.php?table=3">Table de 3</a></p>
-    <p><a href="table.php?table=4">Table de 4</a></p>
-    <p><a href="table.php?table=5">Table de 5</a></p>
-    <p><a href="table.php?table=6">Table de 6</a></p>
-    <p><a href="table.php?table=7">Table de 7</a></p>
-    <p><a href="table.php?table=8">Table de 8</a></p>
-    <p><a href="table.php?table=9">Table de 9</a></p>
-    <p><a href="table.php?table=10">Table de 10</a></p>
+    <?php
+    for($i = 1; $i <= 10; $i++)
+    {
+    ?>
+        <p><a href="table.php?table=<?php echo $i ?>">Table de <?php echo $i ?></a></p>
+    <?php
+    }
+    ?>
 </body>
 </html>
